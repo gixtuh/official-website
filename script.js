fb8c677c4c2fc4f3f8f1a1e1114cad4c83cbfdac
gixtuh-2 <slingshotyt123@gmail.com>
2024-11-03 22:48:17 -0800

    hover

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function clickAudio() {
     click.currentTime = 0;
     click.play();
 }
+
+function hoverAudio() {
+    const hover = document.getElementById("hover")
+    hover.currentTime = 0;
+    hover.play()
+}
