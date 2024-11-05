21b2ed530aab86316be7d2c5501fca1eb5147bcf
gixtuh-2 <slingshotyt123@gmail.com>
2024-11-04 18:08:34 -0800

    hover goes bye bye

--- script.js
+++ script.js
@@ -3,12 +3,3 @@ function clickAudio() {
     click.currentTime = 0;
     click.play();
 }
-
-function hoverAudio() {
-    const hover = document.getElementById("hover")
-    hover.currentTime = 0;
-    hover.play()
-}
-document.querySelectorAll('button').forEach(button => {
-    button.addEventListener('mouseover', hoverAudio);
-}, { once: true });
