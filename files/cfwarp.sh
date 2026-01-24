51cec603b1716099206e72549c1a57336067211d
gixtuh <95630115+gixtuh@users.noreply.github.com>
2026-01-24 22:01:00 +0000

    cfwarp installer idk

---
+++ files/cfwarp.sh
@@ -0,0 +1,3 @@
+curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(. /etc/os-release && printf $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
+apt update && apt install cloudflare-warp
