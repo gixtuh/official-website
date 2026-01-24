0e9eb06a5980de064450b00736cc9d0fd1f15a45
gixtuh <95630115+gixtuh@users.noreply.github.com>
2026-01-24 22:11:15 +0000

    -y

--- files/cfwarp.sh
+++ files/cfwarp.sh
@@ -1,4 +1,4 @@
 echo "linxux bedian cfwarp installer official thing lolol"
 curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(. /etc/os-release && printf $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
-sudo apt install cloudflare-warp
+sudo apt install -y cloudflare-warp
