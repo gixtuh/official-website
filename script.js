d14e9ea6fd137ae2697f2e8cb1b1c4ad4eb7a159
gixtuh <95630115+gixtuh@users.noreply.github.com>
2026-06-30 22:32:46 +0000

    gixtuh.vercel.app 2024-2026

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function clickAudio() {
+/*function clickAudio() {
     const click = document.getElementById("click")
     click.currentTime = 0;
     click.play();
 }
+*/
+alert("gixtuh has shut this webpage down.\nWSRadio servers still live.\nExit this webpage now.")
+window.location.href = "https://www.youtube.com/watch?v=GNsXUj7My5E"
